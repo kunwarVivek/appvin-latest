549cbf779683524499a12a93afb91846c16d826d
Praveen <praveen.kumar@appvintech.com>
2025-05-26 19:38:58 +0530

    mail code

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,6 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
-import nodemailer from 'nodemailer';
+import mail from 'nodemailer';
 import cors from 'cors';
+
+// require("dotenv").config();
+// const express = require('express');
+
+// const mail = require("nodemailer");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,20 +17,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Configure your SMTP credentials here
-const transporter = nodemailer.createTransport({
-  service: 'office365', // or ''
-  host: "smtp.office365.com",
-  port: 587,
+// eslint-disable-next-line no-undef
+const transporter = mail.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
   secure: false,
   auth: {
-    user: 'raj.banka@appvintech.com',
-    pass: '>DA9@E?J',
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
-  tls: {
-    rejectUnauthorized: false
-    //ciphers: 'SSLv3',
-  },
+  debug: true, // Enable debugging
+  logger: true, // Log SMTP interactions
 });
 
 // service: 'hotmail', // or 'office365'
@@ -38,37 +42,39 @@ const transporter = nodemailer.createTransport({
 
 
 app.post('/api/contact', async (req, res) => {
-  const { name, email, subject, message, budget, contact_method,preferred_date, preferred_time } = req.body;
+  const { name, email, subject, message, budget, contact_method, preferred_date, preferred_time } = req.body;
   if (!name || !email || !message || !budget || !contact_method || !preferred_date || !preferred_time) {
     return res.status(400).json({ error: 'Missing required fields' });
   }
   try {
+
     await transporter.sendMail({
-      from: 'sales@appvintech.com',
-      to: 'sales@appvintech.com',
-      subject:'AppVin- Contact Enquiry Form',
-      // text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}\nBudget: ${budget}\nContact Method: ${contact_method}\nPreferred Time: ${preferred_time}`,
-      text: 
-      `Name: ${name}\n` +
-      `Email: ${email}\n` +
-      `Subject: ${subject}\n` +
-      `Message: ${message}\n` +
-      `Budget: ${budget}\n` +
-      `Preferred Contact Method: ${contact_method}\n` +
-      `Preferred Date: ${preferred_date}\n` +
-      `Preferred Time: ${preferred_time}`,
-      replyTo: email,
+      from: process.env.SMTP_EMAIL,
+      to: email,
+      subject: 'AppVin- Contact Enquiry Form',
+      // text: `Name : ${name}\nEmail: ${email}\nMessage: ${message}\nBudget: ${budget}\nContact Method: ${contact_method}\nPreferred Time: ${preferred_time}`,
+      text:
+        `Name: ${name}\n` +
+        `Email: ${email}\n` +
+        `Subject: ${subject}\n` +
+        `Message: ${message}\n` +
+        `Budget: ${budget}\n` +
+        `Preferred Contact Method: ${contact_method}\n` +
+        `Preferred Date: ${preferred_date}\n` +
+        `Preferred Time: ${preferred_time}`,
+      // replyTo: email,
     });
     res.json({ success: true });
 
     await transporter.sendMail({
-      from: 'sales@appvintech.com',
+      from: process.env.SMTP_EMAIL,
       to: email,  // <-- user's email from the form
       subject: 'Thanks for contacting us!',
       text: `Hi ${name},\n\nThank you for reaching out! We have received your query:\n\n"${message}"\n\nWe will get back to you soon.\n\nRegards,\nTeam`,
     });
-  } 
+  }
   catch (err) {
+    console.log('contacts', err);
     res.status(500).json({ error: 'Failed to send email', details: err.message });
   }
 });
@@ -80,10 +86,10 @@ app.post('/api/newsletter', async (req, res) => {
     await transporter.sendMail({
       from: 'sales@appvintech.com',
       to: 'sales@appvintech.com',
-      subject:'New Newsletter Form Submission',
+      subject: 'New Newsletter Form Submission',
       // text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}\nBudget: ${budget}\nContact Method: ${contact_method}\nPreferred Time: ${preferred_time}`,
-      text: 
-      `Email: ${email}\n`,
+      text:
+        `Email: ${email}\n`,
       replyTo: email,
     });
     await transporter.sendMail({
